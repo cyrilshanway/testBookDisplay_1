65a8dca21807b467e700d483917b7c967a082bbd
cyrilshanway <cyrilshanway@gmail.com>
2014-11-24 17:28:08 +0800

    Initial Commit

---
+++ testBookDisplay_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testBookDisplay_1
+//
+//  Created by Cyrilshanway on 2014/11/24.
+//  Copyright (c) 2014年 Cyrilshanway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testBookDisplay_1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testBookDisplay_1
+//
+//  Created by Cyrilshanway on 2014/11/24.
+//  Copyright (c) 2014年 Cyrilshanway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
